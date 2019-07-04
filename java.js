ad98f99b6167b0720d118296e9a00a0b4bfde372
wagnerrafa <wagner_rafa@hotmail.com.br>
2019-07-04 18:08:01 -0300

    adicionar botao hamburguer

--- java.js
+++ java.js
@@ -1,9 +1,9 @@
-decisao = confirm("Está pagina contem spoilers, continuar?");
-if (decisao==true){
-alert ("Você clicou no botão OK,\n"+
-"Não nos responsabilizamos por nada");
+// decisao = confirm("Está pagina contem spoilers, continuar?");
+// if (decisao==true){
+// alert ("Você clicou no botão OK,\n"+
+// "Não nos responsabilizamos por nada");
 
-} else {
-    window.location="https://www.youtube.com/watch?v=L5rwXLYgWl8";
+// } else {
+//     window.location="https://www.youtube.com/watch?v=L5rwXLYgWl8";
 
-}
+// }
